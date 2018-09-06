c5aafff90bca40ee14cab6ea801a3a131f9e81fb
salvadorsuz <salvadorsuz@yahoo.es>
2018-09-06 13:28:48 +0200

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+var path = require('path');
+ var webpack = require('webpack');
+ module.exports = {
+     entry: './js/main.js',
+     output: {
+         path: path.resolve(__dirname, 'build'),
+         filename: 'main.bundle.js'
+     },
+     module: {
+        rules: [
+          {
+            test: /\.js$/,
+            exclude: /(node_modules|bower_components)/,
+            use: {
+              loader: 'babel-loader',
+              options: {
+                presets: ['@babel/preset-env'],
+                plugins: [require('@babel/plugin-proposal-object-rest-spread')]
+              }
+            }
+          }
+        ]
+     },
+     stats: {
+         colors: true
+     },
+     devtool: 'source-map'
+ };
